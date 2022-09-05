f11a96fe5c04881e260ceafa806a1f77f639398a
Culcheese <vxin99@gmail.com>
2022-09-05 18:14:37 -0400

    Update commandLine file

---
+++ test/test.py
@@ -0,0 +1,13 @@
+from email.policy import default
+import unittest
+from pathlib import Path
+
+import code
+
+eg, fails = {}, 0
+
+def setUp():
+    print("hello")
+
+def tearDown():
+    print("hello")
