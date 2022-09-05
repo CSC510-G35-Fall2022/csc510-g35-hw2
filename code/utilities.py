6cc5ef7df8d1ce1d2e227b7d0f5b071cb4ca0cf2
Sanjana <sanjucheerla@gmail.com>
2022-09-05 16:55:20 -0400

    edited sym data and added dep data

--- code/utilities.py
+++ code/utilities.py
@@ -1,5 +1,6 @@
 import csv
 import pprint as pp
+from webbrowser import get
 
 
 csvfile = open('hw2test.csv', 'r')
@@ -11,32 +12,42 @@ numcols = []
 numdict = {}
 symcols = []
 symdict = {}
+depcols = []
+depdict = {}
 
 ct = 0
 for i in datalist[0]:
     if i[0].isupper() and i[-1] != '+' and i[-1] != '-' and i[-1] != ':':
         numcols.append(i)
         numdict[i] = ct
-    if  i[-1] == '-' or i[-1] == '+':
+    if i[0].islower() and i[-1] != '+' and i[-1] != '-' and i[-1] != ':':
         symcols.append(i)
         symdict[i] = ct
+    if  i[-1] == '-' or i[-1] == '+':
+        depcols.append(i)
+        depdict[i] = ct
     ct += 1
 
 
 num_data_list = []
 sym_data_list = []
+dep_data_list = []
 
 for idx in range (0, len(datalist)):
     tmp_num = []
     tmp_sym = []
+    tmp_dep = []
     for x in range (0, len(datalist[idx])):
         if x in list(numdict.values()):
             tmp_num.append(datalist[idx][x])
         if x in list(symdict.values()):
             tmp_sym.append(datalist[idx][x])
+        if x in list(depdict.values()):
+            tmp_dep.append(datalist[idx][x])
     
     num_data_list.append(tmp_num)
     sym_data_list.append(tmp_sym)
+    dep_data_list.append(tmp_dep)
 
 class utilities:
     def get_num_data():
@@ -45,6 +56,10 @@ class utilities:
     def get_sym_data():
         return sym_data_list
 
-    pp.pprint(get_num_data())
-    pp.pprint(get_sym_data())
+    def get_dep_data():
+        return dep_data_list
+
+    pp.pprint(get_num_data()[:10])
+    pp.pprint(get_sym_data()[:10])
+    pp.pprint(get_dep_data()[:10])
 
