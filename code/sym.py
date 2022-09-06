9d053961557b3851ab84b558e94fb3f799915bec
Sanjana <sanjucheerla@gmail.com>
2022-09-06 01:06:27 -0400

    edited sym.py to have mid code

--- code/sym.py
+++ code/sym.py
@@ -1,19 +1,27 @@
-
+import utilities
 class Sym:
     
-    def findMode():
-        mode = ""
-        maxCount = 0
-        dictionary = {"word": 1}
-        for key in dictionary:
-            if (dictionary[key] > maxCount): 
-                maxCount = dictionary[key]
-                mode = key
-        
-        return(dictionary[key], mode)
+    def mid(data_):
 
-    print(findMode())
+        tmp_dict = {}
+        for x in range(0, len(data_[0])):
+            print(data_[0][x])
+            tmp_dict[data_[0][x]] = []
+        print(tmp_dict)
+
+        data_list = []
+        mode_list = []
+        mode_list.append(data_[0])
+        for i in range(1, len(data_)):
+            for j in range (0, len(data_[i])):
+                data_list.append(data_[i][j])
+
+        mode = [(max(set(data_list), key = data_list.count))]
+        mode_list.append(mode)
+        return mode_list
 
 
-
+utilobj=utilities.utilities()
+data = utilobj.get_sym_data()
+print(Sym.mid(data))
 
