7fa2482b10a4c0f30c0bd4a248a70fcdc1e7a655
sourabh pardeshi <11sjpardeshi@gmail.com>
2022-09-06 00:28:03 -0400

    Updating Sym

--- code/sym.py
+++ code/sym.py
@@ -1,6 +1,31 @@
 
 class Sym:
-    
+
+    n=0
+    _has={}
+    def __init__(self,c,s):
+        
+        self.at=c
+        self.name=s
+
+    def add(v):
+        if v != '?':
+            self.n=self.n+1
+            self._has[v]=1+(self.has[v] or 0)
+
+    def fun(p):
+        return p*math.log(p,2)
+        
+
+    def div():
+        e=0
+        for _,n in self._has:
+            if n>0:
+                e=e-fun(n/self.n)
+            return e
+
+
+
     def findMode():
         mode = ""
         maxCount = 0
