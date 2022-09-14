0412c3d1cc54e7a7767328c3c3690f676c0dfd5a
SaailGanesh <saailganesh@gmail.com>
2022-09-13 22:36:13 -0400

    Initialising Data Class

---
+++ code/data.py
@@ -0,0 +1,15 @@
+import utilities
+import num as n
+import sym as s
+import cols
+class data:
+    def __init__(self, src):
+        utilobj=utilities.utilities()
+        cols= 'NIL'
+        rows={}
+        if type(src)==str:
+            print("SRC IS STRING")
+        else:
+            for _ in range(s):
+                self.add(row)
+
